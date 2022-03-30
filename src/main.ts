1e8245ac01f6b18c05e9992502c90633da955f6c
avner <avner@zebra-med.com>
2022-03-31 01:33:58 +0300

    interact with ehter

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import "./style.css";
 import { ethers } from "ethers";
+import ContractAbi from "../build/contracts/TodoList.json";
+
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
@@ -7,6 +9,7 @@ app.innerHTML = `
 `;
 // A Web3Provider wraps a standard Web3 provider, which is
 // what MetaMask injects as window.ethereum into each page
+const contractAddress = "0xFF1Fd887eF9a8dae55c1Cd478803b372f29A17C1";
 const provider = new ethers.providers.Web3Provider((window as any).ethereum);
 
 // The MetaMask plugin also allows signing transactions to
@@ -19,7 +22,25 @@ console.log(signer);
 async function loadAccounts() {
 	// MetaMask requires requesting permission to connect users accounts
 	const accounts = await provider.send("eth_requestAccounts", []);
-  console.log(accounts);
+	console.log(accounts);
+}
+async function getContractByProvider(provider: any) {
+  console.log('getContractByProvider');
+	const { abi } = ContractAbi;
+	const contract = new ethers.Contract(contractAddress, abi, provider);
+  console.log(contract);
+  return contract
+}
+
+async function getContractBySigner(signer: any) {
+  console.log('getContractBySigner');
+	const { abi } = ContractAbi;
+	const contract = new ethers.Contract(contractAddress, abi, signer);
+  console.log(contract);
   
+  return contract
 }
 loadAccounts();
+getContractByProvider(provider);
+getContractBySigner(signer);
+console.log(await provider.getBlockNumber());
