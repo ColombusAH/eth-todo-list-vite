909610f8090d73c86cdf607b6614b6dbbeccbbfa
avner <avner@zebra-med.com>
2022-03-31 00:53:49 +0300

    init

---
+++ contracts/TodoList.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.12;
+
+contract TodoList {
+    uint256 public todosCount = 0;
+
+    struct Todo {
+        uint256 id;
+        string content;
+        bool done;
+    }
+
+    event TodoCreated(uint256 id, string content, bool completed);
+
+    mapping(uint256 => Todo) public todos;
+
+    function createTask(string memory _content) public {
+        todos[todosCount] = Todo(todosCount, _content, false);
+        todosCount = todosCount + 1;
+        emit TodoCreated(todosCount, _content, false);
+    }
+}
